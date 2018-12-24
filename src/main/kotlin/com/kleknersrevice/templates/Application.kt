6e30ed17552821aee5ced8b06f9c5fda22cf3ed0
falloutfire <sobaca@mail.ru>
2018-12-24 15:09:05 +0300

    clean application

--- src/main/kotlin/com/kleknersrevice/templates/Application.kt
+++ src/main/kotlin/com/kleknersrevice/templates/Application.kt
@@ -10,13 +10,3 @@ open class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
-
-/*@Configuration
-open class DatabaseConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    open fun dataSource(): DataSource {
-        return DataSourceBuilder.create().build()
-    }
-}*/
